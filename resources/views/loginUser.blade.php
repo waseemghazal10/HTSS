6656c9a39b6fe647b5747809f452dc834014cdd2
GhazalITG <mGhazal@itgsoftware.com>
2022-05-14 01:49:12 +0300

    login page front end

--- resources/views/loginUser.blade.php
+++ resources/views/loginUser.blade.php
@@ -1,24 +1,29 @@
-<!DOCTYPE html>
-<html>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-<body>
-
-<h1>Welcome to login page</h1>
-
-<form action="" method="" target="_blank" id="gg" class="dd">
-  @csrf
-  <div class="error" style= "color:red"> </div>
-  <label for="email">Email:</label>
-  <input type="text" id="email" name="email"><br><br>
-  <label for="password">Password:</label>
-  <input type="password" id="password" name="password"><br><br>
-  <input type="checkbox" id="remember" name="remember" value="remember">
-  <label for="remember"> Remember Me</label><br>
-  <input type="submit" value="Login">
-</form>
-
-<!-- <p>Click on the submit button, and the form will be submittied using the POST method.</p> -->
-
-</body>
+@include('templates.header')
 <script src="{{asset('js/form.js')}}"></script>
+<link rel="stylesheet" type="text/css" href="{{ asset('css/login.css') }}" />
+<div class="w-100 h-100 loginBackground d-flex m-0">
+  <div class="bg-gray-opacity h-100 ml-auto col-4 align-items-center justify-content-center d-flex p-4">
+    <div class="bg-white h-fit p-5 w-100" id="loginContainer">
+      <h3 class="mb-3">Iyada System</h3>
+      <form action="" method="" target="_blank" id="gg" class="dd ">
+        @csrf
+        <div class="error mb-2 ml-2" style="color:red"></div>
+        <input class="p-2 w-100 px-3" type="text" id="email" name="email" placeholder="Enter Email Address"><br><br>
+        <input class="p-2 w-100 px-3" type="password" id="password" name="password" placeholder="Password"><br><br>
+        <input type="checkbox" id="remember" name="remember" value="remember">
+        <label class="text-muted" for="remember"> Remember Me</label><br>
+        <input class="w-100 btn btn-primary" id="login" type="submit" value="Login">
+      </form>
+      <hr>
+      <button class="w-100 btn btn-danger mb-2"><i class="fa fa-brands fa-google"></i> Login With Google</button>
+      <button class="w-100 btn btn-facebook text-white"><i class="fa fa-brands fa-facebook-f"></i> Login With Facebook</button>
+      <hr>
+      <div class="d-flex justify-content-center w-100 mb-1 text-primary"><a href="#">Forgot Password?</a></div>
+      <div class="d-flex w-100 text-primary"><a href="#" class="mx-auto">Create an Account!</a></div>
+    </div>
+  </div>
+  <!-- <p>Click on the submit button, and the form will be submittied using the POST method.</p> -->
+</div>
+</body>
+
 </html>
