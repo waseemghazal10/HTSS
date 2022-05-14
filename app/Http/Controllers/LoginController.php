323c13788a9bd05c2fd6746a9aa90d1fcdc1736a
waseemghazal10 <waseemghazal10@gmail.com>
2022-05-15 01:50:32 +0300

    test

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
  
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-// use App\Models\User;
+use App\Models\User;
 
 class LoginController extends Controller
 {
@@ -22,14 +22,19 @@ class LoginController extends Controller
 
     public function authenticate(Request $request)
     {
+        return response([
+            'msg' => 'The provided credentials do not match our records.'],401);
+            
         $credentials = $request->validate([
             'email' => ['required', 'email'],
             'password' => ['required'],
         ]);
+        $email = $request->email; 
+        $password = $request->password;
         $remember = $request->remember;
-        error_log($remember);
- 
-        if (Auth::attempt($credentials,$remember)) {
+        $users = User::where('Email',$email)->first(); 
+
+        if($users->PassWord === $password){
             $request->session()->regenerate();
  
             // return redirect()->intended('dashboard');
